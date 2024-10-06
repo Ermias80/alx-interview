a96df2dc2270553f708f0837df854488ae53415f
Ermias80 <ermiasmaz8069@gmail.com>
2024-10-06 20:25:43 +0000

    pascal

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-'''A module that defines working with Pascal's triangle.
-'''
+"""Triangle Triangle"""
+
 
 def pascal_triangle(n):
-    """
-    Returns a list of lists of integers representing the Pascal's triangle of n.
-    """
+    """Triangle"""
     if n <= 0:
         return []
-    
-    triangle = [[1]]  # First row of Pascal's triangle
+    pas = [[1]]
+    for row_number in range(1, n):
+        row = [1]
+        for j in range(1, row_number):
+            element = pas[row_number - 1][j - 1] + pas[row_number - 1][j]
+            row.append(element)
+        row.append(1)
+        pas.append(row)
 
-    for i in range(1, n):
-        row = [1]  # Every row starts with 1
-        for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)  # Every row ends with 1
-        triangle.append(row)
-    return triangle
+    return pas
